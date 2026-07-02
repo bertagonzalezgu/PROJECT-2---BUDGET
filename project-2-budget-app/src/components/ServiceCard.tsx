e47d71be26dd50fcf1e6c28532c6c191225cadc5
Berta González <berta.gonzalez99@gmail.com>
2026-07-02 12:12:52 +0200

    fix(ServiceCard.tsx): Add €, input-checkbox, and accessibility

--- project-2-budget-app/src/components/ServiceCard.tsx
+++ project-2-budget-app/src/components/ServiceCard.tsx
@@ -2,13 +2,16 @@ import type {Service} from "../types/service.types";
 
 export default function ServiceCard(service: Service){
     return( 
-        <section>
+        <li>
             <div>
                 <h1>{service.title}</h1>  
                 <p>{service.description}</p>
             </div>   
-                <span>{service.price}</span>  
-        </section>
-          
+                <span>{service.price}€</span> 
+                <div>
+                    <label htmlFor={`add-service-${service.title}`}>Afegir</label>
+                    <input type="checkbox" id={`add-service-${service.title}`} aria-label={`Afegir servei ${service.title}`}/>
+                </div> 
+        </li>          
     );
 }
