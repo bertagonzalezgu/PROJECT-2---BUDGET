d0b68a72523864c85b098f9ba1436c1720db220a
Berta González <berta.gonzalez99@gmail.com>
2026-07-02 09:54:48 +0200

    feat: Import data and interface

---
+++ project-2-budget-app/src/App.tsx
@@ -0,0 +1,6 @@
+import DataServices from '../data/services.json'
+import { Service } from './types/service.types'
+
+export default function App(){
+    const servicesCards = DataServices.map(() => )
+}
---
+++ project-2-budget-app/src/components/ServiceCard.tsx
@@ -0,0 +1,6 @@
+import { Service } from "../types/service.types";
+
+export default function ServiceCard({title, description, price}: ServiceProps){
+    return(      
+    );
+}
