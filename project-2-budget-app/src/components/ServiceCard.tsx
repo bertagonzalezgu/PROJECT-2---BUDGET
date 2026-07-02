f5816715ce0b9fcaf45b076b94f215910a34a24f
Berta González <berta.gonzalez99@gmail.com>
2026-07-02 11:33:38 +0200

    feat: Add function in component ServiceCard.tsx

--- project-2-budget-app/src/components/ServiceCard.tsx
+++ project-2-budget-app/src/components/ServiceCard.tsx
@@ -1,6 +1,14 @@
-import { Service } from "../types/service.types";
+import type {Service} from "../types/service.types";
 
-export default function ServiceCard({title, description, price}: ServiceProps){
-    return(      
+export default function ServiceCard(service: Service){
+    return( 
+        <section>
+            <div>
+                <h1>{service.title}</h1>  
+                <p>{service.description}</p>
+            </div>   
+                <span>{service.price}</span>  
+        </section>
+          
     );
 }
