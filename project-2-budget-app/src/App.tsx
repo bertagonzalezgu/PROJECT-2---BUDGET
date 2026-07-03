8925c9a8bff1c5c3339d604c87a7a9ac4500fe19
Berta González <berta.gonzalez99@gmail.com>
2026-07-03 10:50:33 +0200

    fix: Add styles in components

--- project-2-budget-app/src/App.tsx
+++ project-2-budget-app/src/App.tsx
@@ -1,14 +1,23 @@
 import DataServices from './data/services.json'
 import ServiceCard from './components/ServiceCard'
 import type { Service } from './types/service.types'
+import bgHeader from './assets/img/bg-header.jpg'
 
 export default function App(){
     const servicesCardsList = DataServices.services.map((element: Service) => 
     <ServiceCard key={element.title} serviceData={element} />)
     
     return (
-        <ul>
-            {servicesCardsList}  
-        </ul> 
+        <>
+        <header className="flex bg-linear-to-bl from-green-300 to-blue-200 rounded-3xl shadow-sm max-w-3xl mx-auto p-24 mb-12 items-center justify-center relative border border-gray-100 overflow-hidden">
+            <img src={bgHeader} alt="Background image" className="absolute inset-0 w-full h-full object-cover opacity-90 pointer-events-none object-top"/>
+            <h1 className="absolute text-3xl font-extrabold text-black justify-center tracking-wide">Aconsegueix la millor qualitat</h1>
+        </header>
+        <main className="max-w-3xl mx-auto">
+           <ul className='list-none p-0 m-0 space-y-6'>
+                {servicesCardsList}  
+            </ul> 
+        </main>
+        </>      
     );
 };
--- project-2-budget-app/src/components/ServiceCard.tsx
+++ project-2-budget-app/src/components/ServiceCard.tsx
@@ -2,15 +2,21 @@ import type {ServiceCardProps} from "../types/service.types";
 
 export default function ServiceCard({serviceData}: ServiceCardProps){
     return( 
-        <li>
-            <div>
-                <h1>{serviceData.title}</h1>  
-                <p>{serviceData.description}</p>
+        <li className="bg-white rounded-3xl shadow-md p-8 my-5 border border-gray-100 flex items-center justify-between max-w-3xl mx-auto">
+            <div className="flex-1 pr-4">
+                <h1 className="text-2xl font-bold text-gray-800 mb-1">{serviceData.title}</h1>  
+                <p className="text-sm text-gray-500 font-normal leading-snug max-w-xs tracking-wide">{serviceData.description}</p>
             </div>   
-                <span>{serviceData.price}€</span> 
-                <div>
-                    <label htmlFor={`add-service-${serviceData.title}`}>Afegir</label>
-                    <input type="checkbox" id={`add-service-${serviceData.title}`} aria-label={`Afegir servei ${serviceData.title}`}/>
+            <div className="flex-1 text-center">
+                <span className="text-3xl font-extrabold text-gray-800">{serviceData.price}
+                <span className="text-xl font-bold ml-1">€</span></span>
+            </div>
+                 
+                <div className="flex items-center justify-end gap-3 flex-1">
+                    <label htmlFor={`add-service-${serviceData.title}`}
+                    className="text-sm font-semibold text-gray-700 cursor-pointer select-none">Afegir</label>
+                    <input type="checkbox" id={`add-service-${serviceData.title}`} aria-label={`Afegir servei ${serviceData.title}`}
+                    className="w-5 h-5 rounded cursor-pointer"/>
                 </div> 
         </li>          
     );
