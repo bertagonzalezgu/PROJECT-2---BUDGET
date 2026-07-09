ccb4eee5819003b0db0bb8df715b387735a4d4e8
Berta González <berta.gonzalez99@gmail.com>
2026-07-09 18:57:09 +0200

    fix(client-form): Add styles in ClientForm component

--- project-2-budget-app/src/components/ClientForm.tsx
+++ project-2-budget-app/src/components/ClientForm.tsx
@@ -11,33 +11,28 @@ export default function ClientForm(){
         })
 
     return (
-        <section>
+        <section className="container rounded-3xl shadow-md p-8 my-5 gap-5 border flex flex-col max-w-3xl mx-auto transition-all duration-200 bg-white border-gray-100">
             <div>
-                <h1>Demanar pressupost</h1>
+                <h1 className="text-2xl font-bold text-gray-800 mb-1">Demanar pressupost</h1>
             </div>
             <form onSubmit={onSubmit}>
-                <div>
-                    <label htmlFor="name"></label>
-                    <input 
+                <div className='flex justify-between'>
+                    <input className="flex items-center gap-3 bg-white px-3 py-1.5 rounded-xl border border-gray-200 shadow-sm"
                     type="text" 
-                    placeholder='Nom' 
+                    placeholder='Nom i cognoms' 
                     {...register("name", 
                     {required: true})}/>
                     {errors.name && <span>*Aquest camp és obligatori</span>}
 
-                    <label htmlFor="surname"></label>
-                    <input type="text" placeholder='Cognoms' {...register("surname", {required: true})}/>
-                    {errors.surname && <span>*Aquest camp és obligatori</span>}
-
-                    <label htmlFor="tel"></label>
-                    <input type="number" placeholder='Telèfon' {...register("tel", {required: true})}/>
+                    <input className="flex items-center gap-3 bg-white px-3 py-1.5 rounded-xl border border-gray-200 shadow-sm" type="number" placeholder='Telèfon' 
+                    {...register("tel", 
+                    {required: true})}/>
                     {errors.tel && <span>*Aquest camp és obligatori</span>}
 
-                    <label htmlFor="email"></label>
-                    <input type="email" 
+                    <input className="flex items-center gap-3 bg-white px-3 py-1.5 rounded-xl border border-gray-200 shadow-sm" type="email" 
                     placeholder='Email' 
-                    {...register("email", {
-                    required: "Aquest camp és obligatori",
+                    {...register("email", 
+                    {required: "Aquest camp és obligatori",
                     pattern: {
                             value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
                             message: "El format del correu no és vàlid"
@@ -46,8 +41,9 @@ export default function ClientForm(){
                     />
                     {errors.email?.message && <span>{errors.email.message}</span>}
                 </div>
-                <div>
-                    <button type='submit'>
+                <br />
+                <div className='flex justify-end'>
+                    <button className="flex items-center gap-3 bg-linear-to-bl from-slate-800 to-blue-500 px-3 py-1.5 rounded-xl border border-blue-800 text-white shadow-sm cursor-pointer" type='submit'>
                         Sol·licitar pressupost
                         <img src={arrowRight} alt="Flecha apuntando a la derecha" />
                     </button>
