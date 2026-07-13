eb21fcb59f1dea65db79fe1c211dc599d96d3ffa
Berta González <berta.gonzalez99@gmail.com>
2026-07-13 13:54:07 +0200

    fix(budget-generator): Add localStorage

--- project-2-budget-app/src/hooks/useBudgetList.ts
+++ project-2-budget-app/src/hooks/useBudgetList.ts
@@ -1,9 +1,14 @@
 import type { Budget } from "../types/budget.types";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function useBudgetList(){
     
-    const [budgets, setBudgets] = useState<Budget[]>([]);
+    const [budgets, setBudgets] = useState<Budget[]>(() => {
+    const stored = localStorage.getItem("budgets")
+    return stored ? JSON.parse(stored) : []
+    });
+
+    useEffect(() => {localStorage.setItem("budgets", JSON.stringify(budgets))}, [budgets]);
 
     function addBudgetToList(newBudget: Budget){
     
