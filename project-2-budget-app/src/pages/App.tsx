775063fc76ade80d63b3a9e94e49a395376124ec
Berta González <berta.gonzalez99@gmail.com>
2026-07-15 11:52:21 +0200

    fix(budget-filters): Move budget list section and filters in App.tsx

--- project-2-budget-app/src/pages/App.tsx
+++ project-2-budget-app/src/pages/App.tsx
@@ -102,31 +102,9 @@ export default function App(){
                 )}
             <PriceCounter total={totalPriceServicesSelected}/>
                 <hr className="mt-6 pt-6 border-t border-blue-200 w-fill"/>
-                <div className="max-w-3xl mx-auto">
-                    <div className="flex flex-row justify-between items-center mb-6">
-                        <div>
-                            <h1 className="text-2xl font-bold text-gray-900">
-                                Pressupostos en curs:
-                            </h1>
-                        </div>
-                        <div className="flex flex-row gap-5 items-center">
-                            <input 
-                                type="text" 
-                                placeholder="Buscar..."
-                                className="bg-white px-3 py-1 rounded-xl border border-gray-200 shadow-sm text-sm focus:outline-none focus:border-gray-400"/>
-                            <ul className="flex flex-row gap-4 text-sm font-medium text-gray-600">
-                                <li className="cursor-pointer hover:text-gray-90 transition-colors">Data</li>
-                                <li className="cursor-pointer text-gray-900 font-bold flex items-center gap-1">
-                                    Import <span className="text-xs">▽</span>
-                                </li>
-                                <li className="cursor-pointer hover:text-gray-90 transition-colors">Nom</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div>
-                        <BudgetList budgets={budgets}/>
-                    </div>
-                </div>  
+                <div>
+                    <BudgetList budgets={budgets}/>
+                </div>
         </main>
         </div>      
     );
