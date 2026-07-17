f9beb926404930959c226860358b705509af9f9a
Berta González <berta.gonzalez99@gmail.com>
2026-07-17 17:56:46 +0200

    fix(sort-filter): Add styles

--- project-2-budget-app/src/components/BudgetList.tsx
+++ project-2-budget-app/src/components/BudgetList.tsx
@@ -7,11 +7,15 @@ interface BudgetListProps{
 
 export default function BudgetList({budgets}: BudgetListProps){
 
-    const {searchTerm, filteredBudgets, handleSortFilter, setSearchTerm} = useSearchFilter(budgets);
+    const {searchTerm, sortField, sortDirection, filteredBudgets, handleSortFilter, setSearchTerm} = useSearchFilter(budgets);
 
     if(budgets.length === 0){
         return <p>No tens cap pressupost en curs.</p>
     }
+
+    const getSortBtn = (field: string) => {
+    const isActive = sortField === field;
+    return `cursor-pointer transition-all duration-200 flex items-center gap-1 select-none ${isActive? "text-blue-600 font-bold scale-105" : "text-gray-500 font-medium hover:text-gray-900"}`};
 
     return(
         <>
@@ -25,11 +29,18 @@ export default function BudgetList({budgets}: BudgetListProps){
                             <div className="flex flex-row gap-5 items-center">
                                 <input type="text" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Buscar..." className="bg-white px-3 py-1 rounded-xl border border-gray-200 shadow-sm text-sm focus:outline-none focus:border-gray-400"/>
                                 <ul className="flex flex-row gap-4 text-sm font-medium text-gray-600">
-                                    <li onClick={() => handleSortFilter("date")} className="cursor-pointer hover:text-gray-90 transition-colors">Data</li>
-                                    <li onClick={() => handleSortFilter("amount")} className="cursor-pointer text-gray-900 font-bold flex items-center gap-1">Import
-                                        <span className="text-xs">▽</span>
-                                    </li>
-                                    <li onClick={() => handleSortFilter("name")} className="cursor-pointer hover:text-gray-90 transition-colors">Nom</li>
+                                    <li onClick={() => handleSortFilter("date")} className={getSortBtn("date")}>
+                                    Data {sortField === "date" && (
+                                        <span className="text-xs">{sortDirection === "asc" ? "▲" : "▼"}</span>
+                                    )}</li>
+                                    <li onClick={() => handleSortFilter("amount")} className={getSortBtn("amount")}>
+                                    Import {sortField === "amount" && (
+                                        <span className="text-xs">{sortDirection === "asc" ? "▲" : "▼"}</span>
+                                    )}</li>
+                                    <li onClick={() => handleSortFilter("name")} className={getSortBtn("name")}>
+                                    Nom {sortField === "name" && (
+                                        <span className="text-xs">{sortDirection === "asc" ? "▲" : "▼"}</span>
+                                    )}</li>
                                 </ul>
                             </div>
                         </div>
